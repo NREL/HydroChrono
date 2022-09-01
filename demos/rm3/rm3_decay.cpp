e04b1b97454f5a8bcea142eaeb34acbd592fb34e
David Ogden <david.ogden.nrel@outlook.com>
2022-09-01 16:38:13 +0100

    added surge and sway added mass to heave for WEC-Sim comparison
    - also changed rm3 dt to 0.01s

--- demos/rm3/rm3_decay.cpp
+++ demos/rm3/rm3_decay.cpp
@@ -68,7 +68,7 @@ int main(int argc, char* argv[]) {
 	// system/solver settings
 	ChSystemNSC system;
 	system.Set_G_acc(ChVector<>(0.0, 0.0, 0.0));
-	double timestep = 0.06;
+	double timestep = 0.01;
 	system.SetTimestepperType(ChTimestepper::Type::HHT);
 	system.SetSolverType(ChSolver::Type::GMRES);
 	system.SetSolverMaxIterations(300);  // the higher, the easier to keep the constraints satisfied.
