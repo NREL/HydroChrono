c5a638a6b856d613e92aa8a568a36a909983df85
Maxime PALLUD <maxime.pallud@gmail.com>
2022-12-30 17:26:37 +0100

    GUI Split in independent class
    Implement helper class to create GUI or standard UI without code duplication
    
    Use only forward declaration
    Factory to create the UI
    put HYDROCHRONO_HAVE_IRRLICHT in public interface of HydroChronoGUI lib because it is used in the public header.
    
    guihelper : remove public dependency to irrlicht
    
    Write a message at end of simulation

--- demos/sphere/sphere_decay.cpp
+++ demos/sphere/sphere_decay.cpp
@@ -1,23 +1,8 @@
 #include <hydroc/helper.h>
 #include <hydroc/hydro_forces.h>
 
-#ifdef HYDROCHRONO_HAVE_IRRLICHT
-    #include "chrono_irrlicht/ChIrrMeshTools.h"
-    #include "chrono_irrlicht/ChVisualSystemIrrlicht.h"
-// Use the main namespaces of Irrlicht
-using namespace irr;
-using namespace irr::core;
-using namespace irr::scene;
-using namespace irr::video;
-using namespace irr::io;
-using namespace irr::gui;
-using namespace chrono::irrlicht;
-#endif
+#include <hydroc/gui/guihelper.h>
 
-#include <chrono>      // std::chrono::high_resolution_clock::now
-#include <filesystem>  // c++17 only
-#include <iomanip>     // std::setprecision
-#include <vector>      // std::vector<double>
 
 #include "chrono/core/ChRealtimeStep.h"
 
@@ -25,47 +10,7 @@ using namespace chrono::irrlicht;
 using namespace chrono;
 using namespace chrono::geometry;
 
-#ifdef HYDROCHRONO_HAVE_IRRLICHT
-// Define a class to manage user inputs via the GUI (i.e. play/pause button)
-class MyActionReceiver : public IEventReceiver {
-  public:
-    MyActionReceiver(ChVisualSystemIrrlicht* vsys, bool& buttonPressed) : pressed(buttonPressed) {
-        // store pointer application
-        vis = vsys;
 
-        // ..add a GUI button to control pause/play
-        pauseButton = vis->GetGUIEnvironment()->addButton(rect<s32>(510, 20, 650, 35));
-        buttonText  = vis->GetGUIEnvironment()->addStaticText(L"Paused", rect<s32>(560, 20, 600, 35), false);
-    }
-
-    bool OnEvent(const SEvent& event) {
-        // check if user clicked button
-        if (event.EventType == EET_GUI_EVENT) {
-            switch (event.GUIEvent.EventType) {
-                case EGET_BUTTON_CLICKED:
-                    pressed = !pressed;
-                    if (pressed) {
-                        buttonText->setText(L"Playing");
-                    } else {
-                        buttonText->setText(L"Paused");
-                    }
-                    return pressed;
-                    break;
-                default:
-                    break;
-            }
-        }
-        return false;
-    }
-
-  private:
-    ChVisualSystemIrrlicht* vis;
-    IGUIButton* pauseButton;
-    IGUIStaticText* buttonText;
-
-    bool& pressed;
-};
-#endif
 
 // the main program to be executed:
 int main(int argc, char* argv[]) {
@@ -81,25 +26,31 @@ int main(int argc, char* argv[]) {
         (DATADIR / "sphere" / "geometry" / "oes_task10_sphere.obj").lexically_normal().generic_string();
     auto h5fname = (DATADIR / "sphere" / "hydroData" / "sphere.h5").lexically_normal().generic_string();
 
-    // system/solver settings
-    ChSystemNSC system;
-    system.Set_G_acc(ChVector<>(0.0, 0.0, -9.81));
-    double timestep = 0.015;
-    system.SetSolverType(ChSolver::Type::GMRES);
-    system.SetSolverMaxIterations(300);  // the higher, the easier to keep the constraints satisfied.
-    system.SetStep(timestep);
-    ChRealtimeStepTimer realtime_timer;
-    double simulationDuration = 40.0;
+	// system/solver settings
+	ChSystemNSC system;
 
-    // some io/viz options
-    bool visualizationOn = false;
-#ifdef HYDROCHRONO_HAVE_IRRLICHT
-    visualizationOn = true;
-#endif
-    bool profilingOn = true;
-    bool saveDataOn  = true;
-    std::vector<double> time_vector;
-    std::vector<double> heave_position;
+	system.Set_G_acc(ChVector<>(0.0, 0.0, -9.81));
+
+	double timestep = 0.015;
+	system.SetSolverType(ChSolver::Type::GMRES);
+	system.SetSolverMaxIterations(300);  // the higher, the easier to keep the constraints satisfied.
+	system.SetStep(timestep);
+	ChRealtimeStepTimer realtime_timer;
+	double simulationDuration = 40.0;
+
+	// Create user interface
+	bool visualizationOn = true;
+
+	std::shared_ptr<hydroc::gui::UI> pui = hydroc::gui::CreateUI(visualizationOn);
+
+	hydroc::gui::UI& ui = *pui.get();
+
+	bool profilingOn = true;
+	bool saveDataOn = true;
+
+	// Output timeseries
+	std::vector<double> time_vector;
+	std::vector<double> heave_position;
 
     // set up body from a mesh
     std::cout << "Attempting to open mesh file: " << body1_meshfame << std::endl;
@@ -111,11 +62,18 @@ int main(int argc, char* argv[]) {
         false   // do not collide
     );
 
-    // define the body's initial conditions
-    system.Add(sphereBody);
-    sphereBody->SetNameString("body1");  // must set body name correctly! (must match .h5 file)
-    sphereBody->SetPos(ChVector<>(0, 0, -1));
-    sphereBody->SetMass(261.8e3);
+	// define the body's initial conditions
+	sphereBody->SetNameString("body1"); // must set body name correctly! (must match .h5 file)
+	sphereBody->SetPos(ChVector<>(0, 0, -1));
+	sphereBody->SetMass(261.8e3);
+
+	// Create a visualization material
+	auto cadet_blue = chrono_types::make_shared<ChVisualMaterial>();
+	cadet_blue->SetDiffuseColor(ChColor(0.3f, 0.1f, 0.1f));
+	sphereBody->GetVisualShape(0)->SetMaterial(0, cadet_blue);
+
+	system.Add(sphereBody);
+
 
     // define wave parameters (not used in this demo)
     // Todo define a way to use TestHydro without hydro_inputs/waves
@@ -124,75 +82,32 @@ int main(int argc, char* argv[]) {
     // my_hydro_inputs.regular_wave_amplitude = 0.022;
     // my_hydro_inputs.regular_wave_omega = 2.10;
 
-    // attach hydrodynamic forces to body
-    std::vector<std::shared_ptr<ChBody>> bodies;
-    bodies.push_back(sphereBody);
-    TestHydro blah(bodies, h5fname, my_hydro_inputs);
+	// attach hydrodynamic forces to body
+	std::vector<std::shared_ptr<ChBody>> bodies;
+	bodies.push_back(sphereBody);
+
+
+	TestHydro blah(bodies, h5fname, my_hydro_inputs);
 
     // for profilingvisualizationOn = false;
     auto start = std::chrono::high_resolution_clock::now();
 
-#ifdef HYDROCHRONO_HAVE_IRRLICHT
-    if (visualizationOn) {
-        // Create a visualization material
-        auto cadet_blue = chrono_types::make_shared<ChVisualMaterial>();
-        cadet_blue->SetDiffuseColor(ChColor(0.3f, 0.1f, 0.1f));
-        sphereBody->GetVisualShape(0)->SetMaterial(0, cadet_blue);
+	// main simulation loop
+	ui.Init(&system, "Sphere - Decay Test"); 
 
-        // create the irrlicht application for visualizing
-        auto irrlichtVis = chrono_types::make_shared<ChVisualSystemIrrlicht>();
+	while (system.GetChTime() <= simulationDuration) {
 
-        irrlichtVis->AttachSystem(&system);
-        irrlichtVis->SetWindowSize(1280, 720);
-        irrlichtVis->SetWindowTitle("Sphere - Decay Test");
-        irrlichtVis->SetCameraVertical(CameraVerticalDir::Z);
-        irrlichtVis->Initialize();
+		if(ui.IsRunning(timestep) == false) break;
+		
+		if (ui.simulationStarted) {
 
-        irrlichtVis->AddLogo();
-        irrlichtVis->AddSkyBox();
-        irrlichtVis->AddCamera(ChVector<>(8, -25, 15), ChVector<>(0, 0, 0));
-        irrlichtVis->AddTypicalLights();
+			// append data to output vector
+			time_vector.push_back(system.GetChTime());
+			heave_position.push_back(sphereBody->GetPos().z());
 
-        // add play/pause button
-        bool buttonPressed = false;
-        MyActionReceiver receiver(irrlichtVis.get(), buttonPressed);
-        irrlichtVis->AddUserEventReceiver(&receiver);
+		}
+	}
 
-        // main simulation loop
-        while (irrlichtVis->Run() && system.GetChTime() <= simulationDuration) {
-            irrlichtVis->BeginScene();
-            irrlichtVis->Render();
-
-            // Add grid to materialize horizontal plane
-            tools::drawGrid(irrlichtVis.get(), 1, 1, 30, 30,
-                            ChCoordsys<>(ChVector<>(0, 0.0, 0), Q_from_AngZ(CH_C_PI_2)), chrono::ChColor(.1f, .1f, .1f),
-                            true);
-
-            irrlichtVis->EndScene();
-            if (buttonPressed) {
-                // step the simulation forwards
-                system.DoStepDynamics(timestep);
-                // append data to std vector
-                time_vector.push_back(system.GetChTime());
-                heave_position.push_back(sphereBody->GetPos().z());
-                // force playback to be real-time
-                // realtime_timer.Spin(timestep);
-            }
-        }
-    } else {
-#endif  // #ifdef HYDROCHRONO_HAVE_IRRLICHT
-        int frame = 0;
-        while (system.GetChTime() <= simulationDuration) {
-            // step the simulation forwards
-            system.DoStepDynamics(timestep);
-            // append data to std vector
-            time_vector.push_back(system.GetChTime());
-            heave_position.push_back(sphereBody->GetPos().z());
-            frame++;
-        }
-#ifdef HYDROCHRONO_HAVE_IRRLICHT
-    }
-#endif
 
     // for profiling
     auto end          = std::chrono::high_resolution_clock::now();
@@ -246,5 +161,6 @@ int main(int argc, char* argv[]) {
         outputFile.close();
     }
 
-    return 0;
+	std::cout << "Simulation finished." << std::endl;
+	return 0;
 }
