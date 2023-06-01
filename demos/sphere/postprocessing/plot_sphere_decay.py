7f81511d0d0d660203b31d46480bb5321a2bc853
David Ogden <david.ogden.nrel@outlook.com>
2023-06-01 13:56:16 +0100

    additional sphere decay comparisons

--- demos/sphere/postprocessing/plot_sphere_decay.py
+++ demos/sphere/postprocessing/plot_sphere_decay.py
@@ -17,8 +17,10 @@ refData = pd.read_excel(refFile, 'sphere_decay_comparison')
 
 # Create figure & plot the data
 fig, ax = plt.subplots(figsize=(10, 6))  # Set the figure size for a larger, more visible plot
-ax.plot(refData['Time (s)'], refData['NREL (CFD)'], '-', label='NREL (CFD)', color='darkblue')
-ax.plot(decayTestData[:,0], decayTestData[:,1]+2, '--', label='HydroChrono', color='forestgreen')
+ax.plot(refData['Time (s)'], refData['NREL (CFD)'], '--', label='NREL (CFD)', color='royalblue')
+ax.plot(refData['Time (s)'], refData['WavEC (Lin)'], '--', label='WavEC (Lin)', color='forestgreen')
+ax.plot(refData['Time (s)'], refData['InWave+H (Lin)'], '--', label='InWave-HOTINT (Lin)', color='darkorange')
+ax.plot(decayTestData[:,0], decayTestData[:,1]+2, '--', label='HydroChrono', color='black', linewidth=1.5)
 
 # Set x and y axis limits
 ax.set_xlim([0, 40])
